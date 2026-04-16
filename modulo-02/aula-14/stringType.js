b36f822cb1dd4967497035871c16289961608230
Bruno Lessa <bruno.lessa.redes.sociais@gmail.com>
2026-04-16 05:21:09 -0300

    curso-typescript-zero-to-hero-2.3

---
+++ modulo-02/aula-14/stringType.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let nomeCompleto = "João Silva";
+console.log(nomeCompleto);
+let funcaoEmpresa = "Desenvolvedor";
+console.log(funcaoEmpresa);
+let nomeEmpresa = "Tech Solutions";
+console.log(nomeEmpresa);
+let dadosFuncionario = `Funcionário: ${nomeCompleto}, Função: ${funcaoEmpresa}, Empresa: ${nomeEmpresa}`;
+console.log(dadosFuncionario);
+//# sourceMappingURL=stringType.js.map
---
+++ modulo-02/aula-14/stringType.ts
@@ -0,0 +1,11 @@
+let nomeCompleto: string = "João Silva";
+console.log(nomeCompleto);
+
+let funcaoEmpresa: string = "Desenvolvedor";
+console.log(funcaoEmpresa);
+
+let nomeEmpresa: string = "Tech Solutions";
+console.log(nomeEmpresa);
+
+let dadosFuncionario: string = `Funcionário: ${nomeCompleto}, Função: ${funcaoEmpresa}, Empresa: ${nomeEmpresa}`;
+console.log(dadosFuncionario);
