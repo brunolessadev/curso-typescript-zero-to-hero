ddafd7fd5d35e8252f7d78e93d23c5c1f9699d7c
Bruno Lessa <bruno.lessa.redes.sociais@gmail.com>
2026-04-16 19:06:03 -0300

    curso-typescript-zero-to-hero-2.5

---
+++ modulo-02/aula-18-tuple/tupleType.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let pessoa;
+pessoa = ['João', 'Desenvolvedor', 30];
+// pessoa = ['30', 'João', 'Desenvolvedor'];           //  {ERRO} A ordem dos tipos deve ser respeitada
+console.log(pessoa);
+let pessoa1;
+pessoa1 = ['Maria', 'Designer', 25];
+console.log(pessoa1[1]);
+let pessoa2 = ['Carlos', 'Analista', 28];
+//pessoa2 = ['Carlos', 'Analista', 28];
+console.log(pessoa2);
+let listaFrutas = ['Maçã', 'Banana', 'Laranja', 'Uva', 'Abacaxi'];
+// Para não precisar definir o número exato de elementos, podemos usar o operador rest (...).
+//Lista de tuplas com stred opratior rest, onde o primeiro elemento é do tipo string e os demais elementos são do tipo string[] (array de strings).
+console.log(listaFrutas);
+//# sourceMappingURL=tupleType.js.map
