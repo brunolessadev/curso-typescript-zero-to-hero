6f56383f3fbc4c9383c0212c1a73d3e52b1b96ff
Bruno Lessa <bruno.lessa.redes.sociais@gmail.com>
2026-04-16 04:38:22 -0300

    curso-typescript-zero-to-hero-2.1

---
+++ modulo-02/aula-10/booleanType.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let tarefaConcluida = true;
+let tarefaPendente = false;
+console.log("Tarefa Concluída:", tarefaConcluida);
+console.log("Tarefa Pendente:", tarefaPendente);
+let concluido = true;
+if (concluido) {
+    console.log(`Tarefa concluída com sucesso!`);
+}
+else {
+    console.log(`Tarefa ainda não foi concluída.`);
+}
+//# sourceMappingURL=booleanType.js.map
---
+++ modulo-02/aula-10/booleanType.ts
@@ -0,0 +1,13 @@
+let tarefaConcluida: boolean = true;
+let tarefaPendente: boolean = false;
+
+console.log("Tarefa Concluída:", tarefaConcluida);
+console.log("Tarefa Pendente:", tarefaPendente);
+
+let concluido: boolean = true;
+
+if (concluido) {
+    console.log(`Tarefa concluída com sucesso!`)
+}else {
+    console.log(`Tarefa ainda não foi concluída.`)
+}
