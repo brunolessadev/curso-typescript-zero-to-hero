5838189f31cb8aa28a1192f80f4c92adcbb62fff
Bruno Lessa <bruno.lessa.redes.sociais@gmail.com>
2026-04-17 18:00:04 -0300

    curso-typescript-zero-to-hero-2.8

---
+++ modulo-02/aula-22-tipo-any/anyTipe.ts
@@ -0,0 +1,21 @@
+/*
+const a: number = 34;
+const b: string = 'Bruno';
+
+const result = a + b;
+
+console.log(result);
+
+let frase;
+frase = 'Olá, tudo bem?';
+console.log(frase);
+
+*/
+
+const fomulario: {[campoFormulario: string]: any} = {
+    nome: 'Bruno',
+    sobrenome: 'Santos',
+    idade: 34,
+}
+
+console.log(fomulario);
