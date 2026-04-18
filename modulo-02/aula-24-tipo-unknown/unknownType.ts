30644f52d1d8482b53a16c5c4580cf557486c08f
Bruno Lessa <bruno.lessa.redes.sociais@gmail.com>
2026-04-18 02:55:15 -0300

    curso-typescript-zero-to-hero-2.9

---
+++ modulo-02/aula-24-tipo-unknown/unknownType.ts
@@ -0,0 +1,24 @@
+/*
+let valorVariavel: unknown;
+
+valorVariavel = true;
+valorVariavel = 123;
+valorVariavel = [];
+valorVariavel = 'Oi, tudo bem?';
+
+console.log(valorVariavel);
+
+
+let valor: unknown;
+let valor1: boolean = valor;
+let valor2: any = valor;
+let valor3: any[] = valor;
+let valor4: string = valor;
+
+*/
+
+let algumaCoisaAny: any;
+let algumaCoisaUnknown: unknown;
+
+console.log(algumaCoisaAny.toFixed());
+console.log(algumaCoisaUnknown.toFixed());
