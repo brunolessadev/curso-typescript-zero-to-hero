7fba722e6b7744a7b6f7040ea22ef28fe8c7a8dc
Bruno Lessa <bruno.lessa.redes.sociais@gmail.com>
2026-04-19 03:08:02 -0300

    curso-typescript-zero-to-hero-3.1

---
+++ modulo-02/aula-28-tipo-nulll-undefined/nullUndefinedType.ts
@@ -0,0 +1,12 @@
+let variavelTesteNull = null;
+
+console.log(variavelTesteNull)
+console.log(typeof variavelTesteNull)
+
+let variavelTesteUndefined;
+
+console.log(variavelTesteUndefined);
+console.log(typeof variavelTesteUndefined);
+
+console.log('Exemplo 01:', null == undefined);
+console.log('Exemplo 01:', null === undefined);
