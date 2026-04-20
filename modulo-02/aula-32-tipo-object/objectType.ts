569271a217fe4b9eda31be874bfb0dd2b13d6575
Bruno Lessa <bruno.lessa.redes.sociais@gmail.com>
2026-04-20 16:06:46 -0300

    curso-typescript-zero-to-hero-3.4

--- modulo-02/aula-32-tipo-object/objectType.ts
+++ modulo-02/aula-32-tipo-object/objectType.ts
@@ -10,7 +10,7 @@ const pessoa = {
 console.log(pessoa);
 
 function onboarding01(funcionario: { nome: string}){
-    return 'Seja bem-vindo' + ' ' + funcionario.nome;
+    return '1 Seja bem-vindo' + ' ' + funcionario.nome;
 }
 console.log(onboarding01({nome: 'Bruno Lessa'}));
 
@@ -21,7 +21,7 @@ interface Pessoa {
 
 function onboarding02(pessoa: Pessoa){
     return (
-        'Seja bem-vindo' + 
+        '2 Seja bem-vindo' + 
         pessoa.nome +
         '!' + 
         'Sua função aqui na empresa será' +
@@ -39,7 +39,7 @@ type Pessoa03 = {
 
 function onborading03(pessoa: Pessoa03){
     return (
-        'Seja bem-vindo' + 
+        '3 Seja bem-vindo' + 
         pessoa.nome +
         '!' + 
         'Sua função aqui na empresa será' +
@@ -63,7 +63,7 @@ interface Pessoa04 {
 
 function onborading04(pessoa: Pessoa04){
     return (
-        'Seja bem-vindo' + 
+        '4 Seja bem-vindo' + 
         pessoa.nome +
         '!' + 
         'Sua função aqui na empresa será' +
@@ -78,3 +78,105 @@ function onborading04(pessoa: Pessoa04){
 }
 
 console.log(onborading04({ nome: 'Bruno Lessa', funcao: 'Estudante', linguagem: 'JavaScript/Typescript', email: 'brunolessa@gmail.com' }));
+
+
+interface Pessoa05 {
+    nome: string;
+    funcao: string;
+    linguagem: string;
+    readonly email : string;
+}
+
+function onborading05(pessoa: Pessoa05){
+    return (
+        '5 Seja bem-vindo' + 
+        pessoa.nome +
+        '!' + 
+        'Sua função aqui na empresa será' +
+        '.' +
+        pessoa.funcao +
+        '.' +
+        'Você trabalhará com a linguagem ' +
+        pessoa.linguagem +
+        '.' +
+        'Seu e-mail será ' +
+        pessoa.email 
+    );
+}
+
+console.log(onborading05({ 
+    nome: 'Bruno Lessa', 
+    funcao: 'Estudante', 
+    linguagem: 'JavaScript/Typescript', 
+    email: 'brunolessa@gmail.com' 
+}));
+
+interface Mae {
+    nome: string;
+}
+
+interface Pai {
+    sobrenome: string;
+}
+
+interface Filha extends Mae, Pai {
+    idade: number;
+}
+
+const filha: Filha = {
+    nome: 'Glaucia',
+    sobrenome: 'Lemos',
+    idade: 35
+}
+
+console.log(filha);
+
+interface Cachorro {
+    tipo:string;
+}
+
+interface Gato {
+    tipo:string;
+}
+
+type Animal = Cachorro & Gato;
+
+/* ===================================================================================== */
+
+type Usuario = {
+    nome: string;
+    email: string;
+}
+
+type Admin = {
+    nome: string;
+    email: string;
+    admin: true;
+}
+
+const usuario: Usuario = {
+    nome: ' Bruno Lessa Usuario',
+    email: 'bruno@gmail.com'
+}
+
+const admin: Admin = {
+    nome: ' Bruno Lessa Admin',
+    email: 'bruno@gmail.com',
+    admin: true
+}
+
+
+function acessarSistema<T>(usuario: T){
+    return usuario;
+}
+console.log(acessarSistema<Usuario>(usuario));
+console.log(acessarSistema<Admin>(admin));
+
+/*
+function acessarSistema(usuario: Usuario): Usuario{
+    return usuario;
+}
+*/
+/*
+console.log(acessarSistema(usuario));
+*/
